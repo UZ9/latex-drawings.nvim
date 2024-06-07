5ae35ea7cdfe72c80d00b768f3f44b00dccc998c
Yerti <36551149+UZ9@users.noreply.github.com>
2024-06-06 23:56:46 -0400

    Add working inkscape figure creation

--- lua/latexdrawings/module.lua
+++ lua/latexdrawings/module.lua
@@ -1,14 +1,16 @@
 local M = {}
 
 M.DIRECTORY_NAME = "Diagrams"
+-- M.CREATE_INKSCAPE_COMMAND = "inkscapecom --actions="file-new; export-area-page; export-filename:diagram.svg; export-do;""
 
 function M.stop_drawing()
 	print("Stopped drawing")
 end
 
 function M.begin_drawing()
-	local function start_inkscape(file_path)
-		local handle = io.popen("inkscape " .. file_path)
+	local function run_command(command)
+		print('Executing "' .. command .. '"')
+		local handle = io.popen(command)
 
 		if handle then
 			local result = handle:read("*a")
@@ -20,15 +22,89 @@ function M.begin_drawing()
 		end
 	end
 
+	local function create_inkscape_file(file_path, file_name)
+		print("Creating " .. file_name .. ".svg at " .. file_path)
+
+		-- TODO: inkscapecom is a windows specific, find some way of making this universal
+		run_command(
+			[[inkscapecom --actions="file-new; export-area-page; export-filename:]]
+				.. file_path
+				.. "/"
+				.. file_name
+				.. [[.svg; export-do;]]
+		)
+	end
+
+	-- Might eventually switch to a listener for this, but unnecessary while this is still sync
+	-- local function start_watching_inkscape_file(file)
+	-- 	print("Starting watch on " .. file)
+	--
+	-- 	local handle = vim.loop.new_fs_event()
+	--
+	-- 	local unwatch_callback = function()
+	-- 		vim.loop.fs_event_stop(handle)
+	-- 	end
+	--
+	-- 	local watch_callback = function(_, _, _)
+	-- 		print("INKSCAPE FILE WAS CHANGED")
+	-- 	end
+	--
+	-- 	local event_callback = function(err, filename, events)
+	-- 		print("Experienced event callback")
+	-- 		if err then
+	-- 			print("ERROR: " .. err)
+	-- 		else
+	-- 			watch_callback(filename, events, unwatch_callback)
+	-- 		end
+	-- 	end
+	--
+	-- 	vim.loop.fs_event_start(handle, file, { watch_entry = false, stat = false, recursive = false }, event_callback)
+	--
+	-- 	return handle
+	-- end
+	-- inkscapecom --export-type="pdf" --export-latex test.svg
+
+	local function add_latex_snippet(figure_name)
+		local snippet_text = {
+			[[\begin{figure}]] .. "[ht]",
+			[[    \centering]],
+			[[    \incfig{]] .. figure_name .. [[}]],
+			[[    \caption{Image Caption}]],
+			[[    \label{fig:]] .. figure_name .. [[}]],
+			[[\end{figure}]],
+		}
+
+		local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+		vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, snippet_text)
+
+		vim.api.nvim_win_set_cursor(0, { row, col })
+	end
+
+	local function start_inkscape(file_path, file_name)
+		-- Attempt to create new inkscape file
+		create_inkscape_file(file_path, file_name)
+
+		-- start_watching_inkscape_file(file_path .. "/" .. file_name .. ".svg")
+
+		run_command("inkscapecom " .. file_path .. "/" .. file_name .. ".svg")
+
+		-- At this point, user has closed inkscape and main thread is no longer locked, we now export into latex
+		run_command("inkscapecom --export-type=PDF --export-latex " .. file_path .. "/" .. file_name .. ".svg")
+
+		-- Insert latex snippet linking to created diagram
+		add_latex_snippet(file_name)
+	end
+
 	local directory_path = vim.fn.getcwd() .. "/" .. M.DIRECTORY_NAME
-	local file_name = "test.svg"
+	local file_name = "test"
 
 	if vim.fn.isdirectory(directory_path) == 0 then
-    print(directory_path .. " not found, so creating it now...")
+		print(directory_path .. " not found, so creating it now...")
 		vim.fn.mkdir(directory_path)
 	end
 
-	start_inkscape(directory_path .. "/" .. file_name)
+	start_inkscape(directory_path, file_name)
 end
 
 function M.handle_stdout(_, data)
